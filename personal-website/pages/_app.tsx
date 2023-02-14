48bc289d7d1805bbd470e5b28bab45f10a2fcdd1
Wyatt  Walsh <wyattowalsh@gmail.com>
2023-02-13 17:39:09 -0800

    adding linktree

--- personal-website/pages/_app.tsx
+++ personal-website/pages/_app.tsx
@@ -1,8 +1,12 @@
-import "../styles/globals.css";
-import type { AppProps } from "next/app";
+import "@fontsource/roboto";
+import "@fontsource/ubuntu";
+import "@fontsource/ubuntu-mono";
 import { config } from "@fortawesome/fontawesome-svg-core";
 import "@fortawesome/fontawesome-svg-core/styles.css";
+import type { AppProps } from "next/app";
+import "../styles/globals.css";
 config.autoAddCss = false;
+
 
 export default function App({ Component, pageProps }: AppProps) {
 	return <Component {...pageProps} />;
---
+++ personal-website/pages/linktree.tsx
@@ -0,0 +1,126 @@
+/* linktree page
+*/
+// -- Imports ------------------------------------------------------------------
+import {
+    faCodepen,
+    faGithub,
+    faKaggle,
+    faLinkedinIn,
+    faMedium,
+    faSpotify,
+    faTwitter
+} from "@fortawesome/free-brands-svg-icons";
+import {
+    faEnvelope,
+    faFileAlt
+} from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import Box from "@mui/material/Box";
+import Link from "@mui/material/Link";
+import Tooltip from "@mui/material/Tooltip";
+import Typography from "@mui/material/Typography";
+import Head from "next/head";
+import Image from "next/image";
+import Logo from "../public/img/profile-pic-square.webp";
+import styles from "./linktree.module.scss";
+
+// -- Data ---------------------------------------------------------------------
+const links = [
+    {
+        name: "Email",
+        url: "mailto:wyattowalsh@gmail.com",
+        icon: faEnvelope,
+        color: "#6a9fb5"
+    },
+    {
+        name: "Resume",
+        url: "/resume.pdf",
+        icon: faFileAlt,
+        color: "#6a9fb5"
+    },
+    {
+        name: "LinkedIn",
+        url: "https://www.linkedin.com/in/wyattowalsh",
+        icon: faLinkedinIn,
+        color: "#0A66C2"
+    },
+    {
+        name: "GitHub",
+        url: "https://www.github.com/wyattowalsh",
+        icon: faGithub,
+        color: "#181717"
+    },
+    {
+        name: "Twitter",
+        url: "https://www.twitter.com/wyattowalsh",
+        icon: faTwitter,
+        color: "#1DA1F2"
+    },
+    {
+        name: "Medium",
+        url: "https://www.medium.com/@wyattowalsh",
+        icon: faMedium,
+        color: "#000000"
+    },
+    {
+        name: "Kaggle",
+        url: "https://www.kaggle.com/wyattowalsh",
+        icon: faKaggle,
+        color: "#20BEFF"
+    },
+    {
+        name: "CodePen",
+        url: "https://www.codepen.io/wyattowalsh",
+        icon: faCodepen,
+        color: "#000000"
+    },
+    {
+        name: "Spotify",
+        url: "https://open.spotify.com/user/122096382?si=7aa4f43249af4e45",
+        icon: faSpotify,
+        color: "#1DB954"
+    },
+];
+
+
+export default function LinkTree() {
+    return (
+        <Box className={styles.container}>
+            <Head>
+                <title>Wyatt Walsh's Links</title>
+                <meta name="description" content="Wyatt Walsh's links to online profiles" />
+                <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+            </Head>
+            <Box className={styles.header}>
+                <Typography  className={styles.name} variant="h1" component="h1">
+                    Wyatt Walsh's Links
+                </Typography>
+                <Box className={styles.avatar}>
+                    <Image
+                        src={Logo}
+                        alt="Wyatt Walsh"
+                        quality={100}
+                        sizes="(max-width: 600px) 65vw,
+                                (max-width: 900px) 50vw,
+                                (max-width: 1200px) 45vw,
+                                (max-width: 1536px) 40vw,
+                                65vw"
+                        fill
+                    />
+                </Box>
+            </Box>
+            <Box className={styles.links}>
+                {links.map((link) => (
+                    <Link href={link.url} key={link.name} underline="hover" target="_blank" rel="noreferrer">
+                        <Tooltip title={link.name} placement="bottom" arrow>
+                            <Box className={styles.link}>
+                                <FontAwesomeIcon className={styles.icon} icon={link.icon} size="4x" color={link.color} />
+                                <Typography className={styles.linkName} variant="h2" component="h2">{link.name}</Typography>
+                            </Box>
+                        </Tooltip>
+                    </Link>
+                ))}
+            </Box>
+        </Box>
+    );
+}
