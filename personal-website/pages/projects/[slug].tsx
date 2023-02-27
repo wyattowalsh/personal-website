cbb5938cb352867ddd71ab1b4dd589d65ff7f03e
Wyatt  Walsh <wyattowalsh@gmail.com>
2023-02-27 15:59:48 -0800

    vercel deploy fixes

--- personal-website/pages/projects/[slug].tsx
+++ personal-website/pages/projects/[slug].tsx
@@ -1,92 +1,131 @@
-import Nav from "../../components/Nav";
-import Footer from "../../components/Footer";
-import Box from "@mui/material/Box";
-import styles from "./[slug].module.scss";
-import Image from "next/image";
-import { faGithub } from "@fortawesome/free-brands-svg-icons";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import Tooltip from "@mui/material/Tooltip";
-import fs from "fs";
-import matter from "gray-matter";
-import path from "path";
-import { MDXRemote, MDXRemoteSerializeResult } from "next-mdx-remote";
-import { serialize } from "next-mdx-remote/serialize";
-import { projectFilePaths, PROJECTS_PATH } from "../../utils/mdxUtils";
-import Project from "../../interfaces/project";
+import Typography from '@mui/material/Typography'
+import fs from 'fs'
+import matter from 'gray-matter'
+import { MDXRemote, MDXRemoteSerializeResult } from 'next-mdx-remote'
+import { serialize } from 'next-mdx-remote/serialize'
+import path from 'path'
+import readingTime from 'reading-time'
+import rehypeAutolinkHeadings from 'rehype-autolink-headings'
+import rehypeFormat from 'rehype-format'
+import rehypeKatex from 'rehype-katex'
+import rehypeStringify from 'rehype-stringify'
+import codeTitle from 'remark-code-title'
+import remarkDefinitionList from 'remark-definition-list'
+import remarkFrontmatter from 'remark-frontmatter'
+import remarkGfm from 'remark-gfm'
+import remarkMath from 'remark-math'
+import remarkMermaid from 'remark-mermaidjs'
+import remarkParse from 'remark-parse'
+import remarkRehype from 'remark-rehype'
+import smartypants from 'remark-smartypants'
+import remarkToc from 'remark-toc'
+import Header from '../../components/BlogHeader'
+import Hero from '../../components/Hero'
+import Layout from '../../components/layouts/blog'
+import type { ProjectData } from '../../interfaces/project'
+import { PROJECTS_PATH, projectFilePaths } from '../../utils/mdxUtils'
+const rehypeFigure = require('rehype-figure')
+const rehypeInline = require('rehype-inline')
+const remarkHint = require('remark-hint')
+const remarkPrism = require('remark-prism')
 
 type Props = {
-	source: MDXRemoteSerializeResult;
-	frontMatter: Project;
-};
-
-export default function ProjectPage({ source, frontMatter }: Props) {
-	return <>
-        <Nav />
-        <main className={styles.main}>
-            <Box sx={{ maxWidth: "70%" }}>
-                <h1>
-                    {frontMatter.title} &nbsp;
-                    <a href={frontMatter.url} target="_blank" rel="noopener noreferrer">
-                        <Tooltip title="Visit the Project on GitHub" arrow>
-                            <FontAwesomeIcon icon={faGithub} />
-                        </Tooltip>
-                    </a>
-                </h1>
-                <Box sx={{ overflow: "hidden" }}>
-                    <Image
-                        src={frontMatter.image}
-                        alt={frontMatter.title}
-                        width={350}
-                        height={350}
-                        sizes="(min-width: 768px) 80px, 60px"
-                        style={{
-                            width: "100%",
-                            height: "auto"
-                        }} />
-                </Box>
-                <MDXRemote {...source} />
-            </Box>
-        </main>
-        <Footer />
-    </>;
+  source: MDXRemoteSerializeResult
+  frontMatter: ProjectData
+  readingTime: {
+    text: string
+    minutes: number
+    time: number
+    words: number
+  }
 }
 
-type Params = {
-	params: Project;
-};
+const components = {
+  Hero,
+  Typography,
+  Header,
+}
 
-export const getStaticProps = async ({ params }: Params) => {
-	const projectFilePath = path.join(PROJECTS_PATH, `${params.slug}.mdx`);
-	const source = fs.readFileSync(projectFilePath);
+export default function ProjectPage({
+  source,
+  frontMatter,
+  readingTime,
+}: Props) {
+  return (
+    <>
+      <MDXRemote {...source} components={components} lazy />
+    </>
+  )
+}
 
-	const { content, data } = matter(source);
+ProjectPage.getLayout = function getLayout(page: React.ReactNode) {
+  return <Layout>{page}</Layout>
+}
 
-	const mdxSource = await serialize(content, {
-		// Optionally pass remark/rehype plugins
-		mdxOptions: {
-			remarkPlugins: [],
-			rehypePlugins: [],
-		},
-		scope: data,
-	});
+export const getStaticProps = async ({
+  params,
+}: {
+  params: { slug: string }
+}) => {
+  const postFilePath = path.join(PROJECTS_PATH, `${params.slug}.mdx`)
+  const source = fs.readFileSync(postFilePath)
 
-	return {
-		props: {
-			source: mdxSource,
-			frontMatter: data,
-		},
-	};
-};
+  const { content, data } = matter(source)
+
+  const mdxSource = await serialize(content, {
+    // Optionally pass remark/rehype plugins
+    mdxOptions: {
+      remarkPlugins: [
+        [remarkRehype],
+        [remarkDefinitionList],
+        [remarkFrontmatter],
+        [remarkGfm],
+        [remarkHint],
+        [remarkParse],
+        [remarkMath],
+        [
+          remarkMermaid,
+          {
+            theme: 'dark',
+            launchOptions: { executablePath: '/usr/bin/chromium-browser' },
+          },
+        ],
+        [remarkPrism],
+        [smartypants],
+        [remarkToc],
+        [codeTitle],
+      ],
+      rehypePlugins: [
+        [rehypeStringify],
+        [rehypeKatex],
+        [rehypeAutolinkHeadings],
+        [rehypeFormat],
+        [rehypeFigure],
+        [rehypeInline],
+      ],
+    },
+  })
+
+  const stats = readingTime(content)
+
+  return {
+    props: {
+      source: mdxSource,
+      frontMatter: data,
+      readingTime: stats,
+    },
+  }
+}
 
 export const getStaticPaths = async () => {
-	const paths = projectFilePaths
-		// Remove file extensions for page paths
-		.map((path) => path.replace(/\.mdx?$/, ""))
-		// Map the path into the static paths object required by Next.js
-		.map((slug) => ({ params: { slug } }));
+  const paths = projectFilePaths
+    // Remove file extensions for page paths
+    .map((path) => path.replace(/\.mdx?$/, ''))
+    // Map the path into the static paths object required by Next.js
+    .map((slug) => ({ params: { slug } }))
 
-	return {
-		paths,
-		fallback: false,
-	};
-};
+  return {
+    paths,
+    fallback: false,
+  }
+}
